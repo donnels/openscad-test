b83c81092b9b1360fb98890b909a074db0cd5e3b
donnels <github@donnellan.de>
2022-12-02 11:02:40 +0100

    reorder test into dirs should only show test

--- openscad/indexer.sh
+++ openscad/indexer.sh
@@ -4,20 +4,37 @@
 rm -f index.adoc 2>/dev/null
 
 #the loop to create the index
-ls *.scad |while read file
-do 
-    short=$(basename -s .scad $file)
-    echo "long: $file short: $short"
-    description="${short}.txt"
-    if [[ -f "$description" ]]; then
-        descriptionText="$(cat $description)"
+for directory in */
+do
+    cd $directory
+    dirDescription="description.txt"       
+    if [[ -f "$dirDescription" ]]; then
+        dirDescriptionText="$(cat $dirDescription)"
     else
-        unset descriptionText
+        unset dirDescriptionText
     fi
-
     cat >>index.adoc <<_EOF_
 
-=== Object - ${short}
+=== Project - $directory
+
+$dirDescriptionText
+
+_EOF_
+
+    ls *.scad |while read file
+    do 
+        short=$(basename -s .scad $file)
+        echo "long: $file short: $short"
+        description="${short}.txt"
+        if [[ -f "$description" ]]; then
+            descriptionText="$(cat $description)"
+        else
+            unset descriptionText
+        fi
+
+        cat >>index.adoc <<_EOF_
+
++=== Object - ${short}
 
 $descriptionText
 
